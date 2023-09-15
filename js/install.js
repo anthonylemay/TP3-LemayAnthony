de8639e7e606b9fc8f70e9a5d1fa9757ed3c145f
anthonylemay <anthony_lemay@hotmail.com>
2023-09-15 01:55:27 -0400

    test - app install button 9

--- js/install.js
+++ js/install.js
@@ -14,24 +14,30 @@ function saveBeforeInstallPromptEvent(evt) {
     }
 
 function installPWA(evt) {
-        deferredInstallPrompt.prompt();
+        if (deferredInstallPrompt) {
+            deferredInstallPrompt.prompt();
 
-        evt.srcElement.setAttribute('hidden', true);
-        installAlert.setAttribute('hidden', true);
+            evt.srcElement.setAttribute('hidden', true);
+            installAlert.setAttribute('hidden', true);
+    
+    
+            // Log user response to prompt.
+            deferredInstallPrompt.userChoice
+                .then((choice) => {
+                    if (choice.outcome === 'accepted') {
+                        console.log("L'usager a installé la PWA via le bouton dédié.", choice);
+                    } else {
+                        console.log("L'usager a refusé d'installer la PWA.", choice);
+                        installButton.removeAttribute('hidden');
+                        installAlert.removeAttribute('hidden');
+                    }
+                    deferredInstallPrompt = null;
+                });
 
-
-        // Log user response to prompt.
-        deferredInstallPrompt.userChoice
-            .then((choice) => {
-                if (choice.outcome === 'accepted') {
-                    console.log("L'usager a installé la PWA via le bouton dédié.", choice);
-                } else {
-                    console.log("L'usager a refusé d'installer la PWA.", choice);
-                    installButton.removeAttribute('hidden');
-                    installAlert.removeAttribute('hidden');
-                }
-                deferredInstallPrompt = null;
-            });
+        } else {
+            console.error('Deferred install prompt is not available');
+        }
+       
     }
 
 function logAppInstalled(evt) {
