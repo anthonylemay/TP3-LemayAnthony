2d53dc14f3e7868ce1f02b4737271828b0abefbe
anthonylemay <anthony_lemay@hotmail.com>
2023-09-15 01:11:59 -0400

    test - app install button 2

--- js/install.js
+++ js/install.js
@@ -7,29 +7,28 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 window.addEventListener('appinstalled', logAppInstalled);
 
 function saveBeforeInstallPromptEvent(evt) {
-    // CODELAB: Add code to save event & show the install button.
-        deferredInstallPrompt = evt;
-        installAlert.removeAttribute('hidden');
-        installButton.removeAttribute('hidden');
-    }
+    // Save the beforeinstallprompt event and show the install alert
+    deferredInstallPrompt = evt;
+    installAlert.removeAttribute('hidden');
+}
 
 function installPWA(evt) {
-        deferredInstallPrompt.prompt();
+    deferredInstallPrompt.prompt();
 
-        evt.srcElement.setAttribute('hidden', true);
+    // Hide the source element of the event. If the event source is the installButton, it'll hide the button.
+    evt.srcElement.setAttribute('hidden', true);
 
-
-        // Log user response to prompt.
-        deferredInstallPrompt.userChoice
-            .then((choice) => {
-                if (choice.outcome === 'accepted') {
-                    console.log("L'usager a installé la PWA via le bouton dédié.", choice);
-                } else {
-                    console.log("L'usager a refusé d'installer la PWA.", choice);
-                }
-                deferredInstallPrompt = null;
-            });
-    }
+    // Log user response to prompt.
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log("L'usager a installé la PWA via le bouton dédié.", choice);
+            } else {
+                console.log("L'usager a refusé d'installer la PWA.", choice);
+            }
+            deferredInstallPrompt = null;
+        });
+}
 
 function logAppInstalled(evt) {
     console.log("L'application High Jack a été installée.", evt);
